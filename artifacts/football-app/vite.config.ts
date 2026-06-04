a0f17629c53d651572bc58ff4234f5b840c1d6c4
Azzam Adam <alzamadam4426@gmail.com>
2026-06-05 03:38:08 +0500

    hardcode API URL

--- artifacts/football-app/vite.config.ts
+++ artifacts/football-app/vite.config.ts
@@ -10,4 +10,7 @@ export default defineConfig({
     alias: { "@": path.resolve(import.meta.dirname, "src") },
     dedupe: ["react", "react-dom"],
   },
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify("https://f7080111-5f9c-4a91-8177-216330c4340e-00-2p99b3y7q053z.sisko.replit.dev")
+  }
 });
